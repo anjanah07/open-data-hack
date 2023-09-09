33eb911253594c789d2d572aa5667dba3543756f
harry-sketch <harshbisht90@gmail.com>
2023-09-09 21:51:24 +0530

    setup new repo for web

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type pageProps = {};
+
+const page: React.FC<pageProps> = () => {
+  return <div className="text-red-400">Have a good coding</div>;
+};
+export default page;
